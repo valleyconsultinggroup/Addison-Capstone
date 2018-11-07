2033663372189d7c2a9b1e3c00a2530508092140
Addison Chan <addison95132@gmail.com>
2018-11-07 15:51:24 -0800

    added automated functionality, requirements.txt

--- README.md
+++ README.md
@@ -6,13 +6,33 @@ Github Username: addcninblue
 
 Landing site: i'll make this available as a web app later on
 
+### Setup
+
+`virtualenv . -p python3` to set up the virtualenv
+`pip install -r requirements.txt` to install all requirements (pip modules)
+main file: emails.py
+
 ### Usage
 
-Use Linkedin for names
+Linkedin:
+- Search up recruiters
+- SCROLL ALL THE WAY DOWN for page to fully load
+- Ctrl-S the whole website as a mhtml
+- Put in `linkedin` folder with file format `{domain}_{number}.mhtml`
+  - For example: `valleconsultinggroup.com_1.mhtml`
+- Repeat for all your pages
 
-Use rocketreach.co for email formats
+Rocketreach:
+- Search up {website} email format on Google, and go to rocketreach site
+- Ctrl-S the whole website as a mhtml
+- Put in `rocketreach` folder with file format `{domain}.mhtml`
+  - For example: `valleconsultinggroup.com.mhtml`
+- Repeat for each domain
 
-main file: `emails.py`
+Running the script:
+- Remember to source first: `source bin/activate`
+- Run the script: `python emails.py`
+- Emails will be printed
 
 ### Revision History
 
@@ -33,9 +53,9 @@ This script hopes to change that. It automates the most tedious parts of it, all
 | 1 | Analyzing tools available for webscraping / email server pinging | 1 Hour | 11/01/2018 |  |
 | 2 | Create basic functionality with manual/hard-coded emails + email server pinging api hack | 1 Hour | 11/01/2018 | |
 | 3 | Create basic functionality with hard-coded emails + automated server pinging | 1 Hours | 11/01/2018 |  |
-| 4 | Create functionality with scraping emails from saved websites | 2 Hours | |  |
-| 5 | Create functionality with converting email formats from website format | 1 Hour | |  |
-| 6 | Create frontend/website for scraping emails | 4 Hours | |  |
+| 4 | Create functionality with scraping emails from saved websites | 2 Hours | 11/07/2018 |  |
+| 5 | Create functionality with converting email formats from website format | 1 Hour | 11/07/2018 |  |
+| 6 | Create frontend/website for scraping emails | ? Hours | Looking to implement in the future / after dev |  |
 | 7 | Use Selenium to automate scraping | ? Hours | | Looking to implement in the future / after dev |
 
 ### Technical Stack
--- emails.py
+++ emails.py
@@ -2,19 +2,26 @@ import socket
 import smtplib
 import dns.resolver
 import re
+import os
 
 class Name():
     def __init__(self, name):
-        self.first, self.last = name.lower().split(" ")
-        self.first_initial = self.first[0]
-        self.last_initial = self.last[0]
+        name_parts = name.lower().split(" ")
+        if len(name_parts) == 2:
+            self.first, self.last = name_parts
+        elif len(name_parts) == 3:
+            self.first, self.last = name_parts[0], name_parts[2]
+        else:
+            self.first = ""
+            self.last = ""
+        self.first_initial = self.first[0:1]
+        self.last_initial = self.last[0:1]
 
 def check_email(addressToVerify):
     match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', addressToVerify)
 
     if match == None:
-        print('Bad Syntax')
-        raise ValueError('Bad Syntax')
+        print('Bad Syntax', addressToVerify)
 
     index = addressToVerify.find("@")
     domain = addressToVerify[index+1:]
@@ -42,20 +49,115 @@ def check_email(addressToVerify):
     else:
         return False
 
+def get_emails_from_folder(folder_name):
+    """
+    Returns names of files in a folder
+    mapping: company name (str) -> list of names ([str])
+    """
+    files = os.listdir(folder_name)
+    company_to_file = {}
+    for file in files:
+        file_parts = file.rsplit(".", 1)
+        if len(file_parts) != 2:
+            continue
+        filename, extension = file_parts
+        if extension != "mhtml":
+            continue
+        company, number = file.split("_")
+        company_files = company_to_file.get(company, [])
+        company_to_file[company] = company_files + [file]
+    return company_to_file
+
+def get_formats_from_folder(folder_name):
+    """
+    Returns names of files in a folder
+    mapping: company name (str) -> filename (str)
+    """
+    files = os.listdir(folder_name)
+    company_to_file = {}
+    for file in files:
+        file_parts = file.rsplit(".", 1)
+        if len(file_parts) != 2:
+            continue
+        filename, extension = file_parts
+        if extension != "mhtml":
+            continue
+        company_to_file[filename] = file
+    return company_to_file
+
+def email_file_to_names(filename):
+    """
+    Returns names from a linkedin page
+    """
+    with open(filename) as f:
+        return re.findall('actor-name">([a-zA-z ]*)<', f.read())
+
+def format_file_to_formats(filename):
+    """
+    Returns top 3 email formats of a company given the file
+    """
+    with open(filename) as f:
+        results = re.findall('ng-binding">([^@%\n]*)<', f.read())
+        print(results)
+        return results[2:]
+
+def construct_email_formats(formats, domain):
+    def formatted_email(n):
+        email_styles = []
+        for email_format in formats:
+            parts = email_format.split(" ")
+            if len(parts) == 3 and parts[1] in ["'_'", "'.'", "'-'"]:
+                first_part, middle_part, second_part = parts
+                middle_part = middle_part.strip("'")
+            elif len(parts) == 2:
+                first_part, second_part = parts
+                middle_part = ""
+            elif len(parts) == 1:
+                first_part = parts[0]
+                middle_part = ""
+                second_part = ""
+            else:
+                print(email_format)
+                continue
+            email_style = '{first_part}{middle_part}{second_part}@{domain}'.format(
+                first_part=getattr(n, first_part, ""),
+                middle_part=middle_part,
+                second_part=getattr(n, second_part, ""),
+                domain=domain,
+            )
+            email_styles.append(email_style)
+        return email_styles
+    return formatted_email
+
 def main():
-    # use rocketreach
-    formatted_email = lambda n: [
-        f'{n.first_initial}{n.last}@nvidia.com',
-        f'{n.first}{n.last}@nvidia.com',
-        f'{n.first}{n.last_initial}@nvidia.com'
-    ]
-    # names = ["Daniel Yang", "Dan Patel", "Ashley Chu", "Jennifer Jones", "Haig White", "Isabel Sarkis", "Julia Tyson"]
-    # names = ["Craig Giraudo", "Bennett Yang", "Edwin Dong", "Ashley Jetson", "Shane Thrett", "Marilyn Ibanez", "Jessica Verley", "Vimu Rajdev", "Dayna Wu"]
-    # names = ["Sandeep Tripathi", "Floyd Clark", "Sylvia Li", "Ajaypaul Singh", "Katherine Nguyen", "Lauren Silveira", "Tiffany Landayan", "Prashant Srivatsa"]
-    for name in names:
-        n = Name(name)
-        for email in formatted_email(n):
+    emails_folder = "linkedin"
+    formats_folder = "rocketreach"
+    emailfiles = get_emails_from_folder(emails_folder)
+    formatfiles = get_formats_from_folder(formats_folder)
+    possible_emails = []
+    for domain, files in emailfiles.items():
+        # check for catch-all domains
+        if check_email(f'asdfasdfasdfadsfasdfasdf@{domain}'):
+            print(f'{domain} has a catch all, so we cannot get emails')
+            continue
+        names = []
+        for file in files:
+            names += email_file_to_names(emails_folder + "/" + file)
+        formats = format_file_to_formats(f'{formats_folder}/{domain}.mhtml')
+        formatted_email = construct_email_formats(formats, domain)
+        for name in names:
+            if "linkedin" in name.lower():
+                continue
+            possible_emails += formatted_email(Name(name))
+    print(possible_emails)
+    working_emails = []
+    try:
+        for email in possible_emails:
             if check_email(email):
                 print(email)
+                working_emails.append(email)
+    except KeyboardInterrupt:
+        print(working_emails)
+    return
 
-# main()
+main()
